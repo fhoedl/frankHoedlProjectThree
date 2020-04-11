6d7d82234c883423f5086dc8fc08e3897863e8d3
Frank Hoedl <frankhoedl@mac.com>
2020-04-11 17:08:03 -0400

    added wrapper, initial media queries

--- .gitignore/scriptsWithShuffleREF.js
+++ .gitignore/scriptsWithShuffleREF.js
@@ -3,7 +3,7 @@ const rabbitApp = {};
 
 // ARRAY... Questions w/ options & answers
 rabbitApp.questionsArray = [
-    {   
+    {
         title: `Question 1`,
         question: `Tom, Dick or Harry? no...`,
         option1: `Tom`,
@@ -33,6 +33,7 @@ rabbitApp.questionsArray = [
         answerURL1: `...link to "Rocky Horror"`,
         answerURL2: `...link to bat out of hell content`,
         answerURL3: `...link to "Fight Club"`,
+
     },
     {
         title: `Question 4`,
@@ -47,7 +48,7 @@ rabbitApp.questionsArray = [
         I don’t much care where–‘ said Alice. 
         Then it doesn’t matter which way you go, said the Cat."`,
     }
-    ]
+]
 
 // SHUFFLING ARRAY randomly
 rabbitApp.shuffleQuestion = function shuffle(array) {
@@ -64,7 +65,7 @@ rabbitApp.shuffleQuestion = function shuffle(array) {
 
 // ...on event, CALLS Question
 rabbitApp.getQuestion = () => {
-    $('.logo, h1').on('click', (e) =>{
+    $('.logo, h1').on('click', (e) => {
         e.preventDefault();
         $('div.logo').css('transform', 'scale(0)').toggleClass('landingElement', 1000, `easeInSine`);
         console.log(rabbitApp.questionsArray[0].question, `...remove log`);
@@ -75,7 +76,7 @@ rabbitApp.getQuestion = () => {
 
 // init FUNCTION Calls
 rabbitApp.init = () => {
-    rabbitApp.shuffleQuestion(rabbitApp.questionsArray)  
+    rabbitApp.shuffleQuestion(rabbitApp.questionsArray)
     rabbitApp.getQuestion();
 }
 
@@ -83,36 +84,3 @@ rabbitApp.init = () => {
 $(() => {
     rabbitApp.init();
 })
-
-
-
-// ********************************************************************* 
-// PSEUDO CODE, as approved...
-// "Rabbit Hole" OR "All the places you'll go..." HTML/CSS LANDING Page
-
-// WAIT for Document Ready
-// Create Object 
-    // ...containing all 8 questions
-    // ...for each question, create 2-4 options
-
-// Create Object 
-    // ...containing correlated answers (link for selected option)
-
-// LISTENER, on SUBMIT event 
-// RANDOMLY select from "questions" Object
-    // ...interact with DOM to display the first question
-    // ...and display radial buttons for user response
-
-// LISTENER, for user response, store as VALUE
-    // ...using VALUE, select from corresponding "options" Object
-    // ...using selection, store VALUE within "answer" Object
-
-// LOOP thru "questions"
-    // ...RANDOMLY select next question, 4 of 8 questions(for variety)
-    // ...interact with DOM, present each question & user options
-    // ...LISTENER, for user response, store each VALUE 
-    // ...using VALUE, select from corresponding "options" Object
-    // ...using selection, store VALUE within "answer" Object
-
-// LISTENER, for SUBMIT, once six questions asked  
-    // ...interact with DOM, based on "answer" Object
