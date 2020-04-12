9d5e7ba22cd60ba2a902c0e167df17e92a27027c
Frank Hoedl <frankhoedl@mac.com>
2020-04-11 22:59:43 -0400

    mobile tweak

--- scripts.js
+++ scripts.js
@@ -66,10 +66,8 @@ rabbitApp.shuffleQuestion = function shuffle(array) {
 rabbitApp.getQuestion = () => {
     $('.lrgLogo, h1').on('click', (e) =>{
         e.preventDefault();
-        $('div.lrgLogo')
-            .css('transform', 'scale(4)', '1500ms')
-            .toggleClass('landingElement', 1000, `easeInSine`);
-
+        $('div.lrgLogo').css('transform', 'scale(4)', '1500ms')
+            .toggleClass('landingElement', 1500, `easeInSine`);
         console.log(rabbitApp.questionsArray[0].question, `...remove log`);
         console.log(rabbitApp.questionsArray[1].question, `...remove log`);
         console.log(rabbitApp.questionsArray[2].question, `...remove log`);
