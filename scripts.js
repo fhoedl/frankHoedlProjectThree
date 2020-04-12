c862661412dc306674d650188efe30e0153b4139
Frank Hoedl <frankhoedl@mac.com>
2020-04-11 23:18:55 -0400

    mobile testing

--- scripts.js
+++ scripts.js
@@ -66,8 +66,9 @@ rabbitApp.shuffleQuestion = function shuffle(array) {
 rabbitApp.getQuestion = () => {
     $('.lrgLogo, h1').on('click', (e) =>{
         e.preventDefault();
-        $('div.lrgLogo').css('transform', 'scale(4)', '250ms')
-            .toggleClass('landingElement', 100, `easeInSine`);
+        $('div.lrgLogo')
+            // .css('transform', 'scale(4)', '250ms')
+            .toggleClass('landingElement', 1000, `easeInSine`);
         console.log(rabbitApp.questionsArray[0].question, `...remove log`);
         console.log(rabbitApp.questionsArray[1].question, `...remove log`);
         console.log(rabbitApp.questionsArray[2].question, `...remove log`);
